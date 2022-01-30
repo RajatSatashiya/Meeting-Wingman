f4e6fd82acbf7cafe472ce43dd44f3f09ffd8c96
Rajat Satashiya <kshri4398@gmail.com>
2022-01-30 19:37:45 +0530

    made change

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -2,22 +2,26 @@ const router = require("express").Router();
 const client = require("twilio")();
 
 router.post("/send", (req, res) => {
-  const { name, phone, keyword } = req.body;
-  console.log(req.body);
+  try {
+    const { name, phone, keyword } = req.body;
+    console.log(req.body);
 
-  client.messages
-    .create({
-      body: `Meeting Wingman: ${name} please check your meeting, your specified keyword: ${keyword} was highlighted during the meet`,
-      from: "whatsapp:+14155238886",
-      to: `whatsapp:${phone}`,
-    })
-    .then((message) => console.log(message.sid))
-    .then((message) => res.send("message sent!!"))
-    .done();
+    client.messages
+      .create({
+        body: `Meeting Wingman: ${name} please check your meeting, your specified keyword: ${keyword} was highlighted during the meet`,
+        from: "whatsapp:+14155238886",
+        to: `whatsapp:${phone}`,
+      })
+      .then((message) => console.log(message.sid))
+      .then((message) => res.send("message sent!!"))
+      .done();
 
-  res.json({
-    name: "jetha",
-  });
+    res.json({
+      message: "message was send",
+    });
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 module.exports = router;
