3c1c35fb300caaa159bf930606418e637c06b331
Ujjwal Gupta <ujjwalgupta3108@gmail.com>
2022-01-30 18:47:15 +0530

    minor change in route

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -1,17 +1,18 @@
 const router = require("express").Router();
 const client = require("twilio")();
 
-router.get("/send/:id", (req, res) => {
-  const sender = req.params.id;
-  client.messages
-    .create({
-      body: `Your name was called during the meeting at 11:52AM ${sender}`,
-      from: "whatsapp:+14155238886",
-      to: "whatsapp:+918924805069",
-    })
-    .then((message) => console.log(message.sid))
-    .then((message) => res.send("message sent!!"))
-    .done();
+router.get("/send", (req, res) => {
+  const { sender } = req.body;
+  console.log(sender);
+  // client.messages
+  //   .create({
+  //     body: `Your name was called during the meeting at 11:52AM ${sender}`,
+  //     from: "whatsapp:+14155238886",
+  //     to: "whatsapp:+918924805069",
+  //   })
+  //   .then((message) => console.log(message.sid))
+  //   .then((message) => res.send("message sent!!"))
+  //   .done();
 });
 
 module.exports = router;
